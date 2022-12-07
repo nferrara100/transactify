119cdbd3bd76199bc1e8a3a6cb5a4fb784953d45
Nicholas Ferrara <nferrara100@gmail.com>
2022-12-07 17:40:49 +0000

    feat: create transaction endpoint

--- api/transactions.php
+++ api/transactions.php
@@ -26,13 +26,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $url = $endpoint . "?" . http_build_query($parameters);
         $response = file_get_contents($url, false, $context);
 
-        // if $response does not return a 200 status code then return an error
         if ($response === false) {
             http_response_code(500);
             echo json_encode(
                 array(
                     "error" => "Internal Server Error",
-                    "message" => "An error occurred while trying to authenticate the user."
+                    "message" => "An error occurred."
                 )
             );
             exit();
@@ -58,7 +57,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             echo json_encode(
                 array(
                     "error" => "Internal Server Error",
-                    "message" => "An error occurred while trying to authenticate the user."
+                    "message" => "An error occurred.",
+                    "error_code" => $json_response["jsonCode"]
                 )
             );
             exit();
@@ -77,6 +77,78 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     } else {
         http_response_code(403);
     }
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_COOKIE["authToken"])) {
+        // Check if username and password match the username and password in .env
+        $endpoint = "https://www.expensify.com/api";
+        $parameters = array(
+            "command" => "CreateTransaction",
+            "authToken" => $_COOKIE["authToken"],
+            "created" => $_POST["created"],
+            "amount" => $_POST["amount"],
+            "merchant" => $_POST["merchant"],
+        );
+        $options = array(
+            'http' => array(
+                'method' => 'POST',
+            )
+        );
+        $context = stream_context_create($options);
+        $url = $endpoint . "?" . http_build_query($parameters);
+        $response = file_get_contents($url, false, $context);
+
+        if ($response === false) {
+            http_response_code(500);
+            echo json_encode(
+                array(
+                    "error" => "Internal Server Error",
+                    "message" => "An error occurred."
+                )
+            );
+            exit();
+        }
+
+        // Decode the response
+        $json_response = json_decode($response, true);
+
+        if ($json_response["jsonCode"] === 407) {
+            http_response_code(401);
+            echo json_encode(
+                array(
+                    "error" => "Auth token not valid",
+                    "message" => "Please log in again."
+                )
+            );
+            exit();
+        }
+
+        // if $response is not a 200 status code then return an error
+        if ($json_response["jsonCode"] !== 200) {
+            http_response_code(500);
+            echo json_encode(
+                array(
+                    "error" => "Internal Server Error",
+                    "message" => "An error occurred.",
+                    "error_code" => $json_response["jsonCode"]
+                )
+            );
+            exit();
+        }
+
+        // if $response is a 200 status code then return the authToken
+        if ($json_response["jsonCode"] === 200) {
+            http_response_code(200);
+            echo json_encode(
+                array(
+                    "transactions" => $json_response["transactionList"]
+                )
+            );
+            exit();
+        }
+    } else {
+        http_response_code(403);
+    }
+
 } else if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     // If the request is an options request return a status 200
     http_response_code(200);
