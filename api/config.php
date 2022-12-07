2d47e65891c770ff9d2991b61fb4c1711c605627
Nicholas Ferrara <nferrara100@gmail.com>
2022-12-07 17:16:38 +0000

    fix: auth cookie expires after 2 hours

--- api/config.php
+++ api/config.php
@@ -4,6 +4,7 @@ $GLOBALS['config'] = array(
     'partnerName' => getenv('partnerName'),
     'partnerPassword' => getenv('partnerPassword'),
     'auth_cookie' => array(
-        'expiry' => 86400
+        // Actually expires after 2 hours, but shorter here to avoid edge cases
+        'expiry' => 60 * 115
     ),
 );
