910834c47e49f732175348510fba5743c79c073e
Nicholas Ferrara <nferrara100@gmail.com>
2023-01-08 01:24:04 +0000

    fix: committed env details which were not in env

--- api/config.php
+++ api/config.php
@@ -1,8 +1,8 @@
 <?php
 
 $GLOBALS['config'] = array(
-    'partnerName' => getenv('partnerName'),
-    'partnerPassword' => getenv('partnerPassword'),
+    'partnerName' => "applicant",
+    'partnerPassword' => "d7c3119c6cdab02d68d9",
     'auth_cookie' => array(
         // Actually expires after 2 hours, but shorter here to avoid edge cases
         'expiry' => 60 * 115
