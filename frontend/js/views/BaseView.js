nferrara100/transactify

commit bc41da78208f7cc1f0eb0232810d67785c3d38f9

feat: mobile friendly

frontend/js/views/BaseView.js
@@ -22,13 +22,12 @@ export class BaseView {
         document.querySelector("#modal").innerHTML = `
             <div class="modal-background">
                 <div class="modal-foreground ${small ? "small-modal" : ""}">
-                    <div class="modal-top">
-                        <hr>
-                        <span id="modal-close" class="close">&times;</span>
-                    </div>
+                    <hr>
+                    <span id="modal-close" class="close">&times;</span>
                     <div id="modal-insert">${html}</div>
                 </div>
-            </div>`;
+            </div>
+        `;
         document
             .querySelector("#modal-close")
             .addEventListener("click", () => this.navigateTo("/"));

frontend/styles.css
@@ -170,15 +170,10 @@ table td {
     max-width: 365px;
 }
 
-.modal-top {
-    display: block;
-    overflow: hidden;
-}
-
 hr {
     border: 0;
     height: 5px;
-    width: 80%;
+    width: calc(90% - 20px); /* save room for close button if applicable */
     float: left;
     background-image: linear-gradient(90deg, var(--bright-orange), transparent);
 }
@@ -244,7 +239,7 @@ label {
 
 .title {
     display: inline-block;
-    margin-bottom: 12px;
+    margin-bottom: 10px;
 }
 
 .title-text {
@@ -255,6 +250,7 @@ label {
 .subtitle {
     font-size: 1.25em;
     color: var(--dimmed-text-color);
+    white-space: nowrap;
 }
 
 #page {
@@ -365,5 +361,39 @@ thead {
     }
     100% {
         opacity: 1;
+    }
+}
+
+nav {
+    margin-bottom: 10px;
+    overflow: hidden;
+}
+
+.nav-buttons {
+    display: inline;
+}
+
+@media (max-width: 1100px) {
+    .nav-buttons {
+        display: block;
+        margin: auto;
+        overflow: hidden;
+    }
+
+    .nav-buttons .button {
+        float: left;
+    }
+
+    .nav-buttons .button:first-child {
+        margin-left: 0;
+    }
+}
+
+@media (max-width: 550px) {
+    .modal-foreground {
+        width: calc(100% - 48px);
+    }
+    input {
+        width: 300px;
     }
 }

frontend/js/views/ListTransactions.js
@@ -22,13 +22,17 @@ export class ListTransactions extends BaseView {
             return;
         }
         fillPage(`
-        <div class="title">
-        <img src = "/frontend/icon.svg" alt="Logo" id="logo"/>
-        <span class="title-text">TRANSACTIFY</span>
-        <span class="subtitle">Manage your transactions. Easily.</span>
-        </div>
-        <a class="button secondary-button" ajax-link href="/logout">Logout</a>
-        <a href="/create" class="button" ajax-link>+ Create Transaction</a>
+        <nav>
+            <div class="title">
+            <img src = "/frontend/icon.svg" alt="Logo" id="logo"/>
+            <span class="title-text">TRANSACTIFY</span>
+            <span class="subtitle">Manage your transactions. Easily.</span>
+            </div>
+            <div class="nav-buttons">
+                <a class="button secondary-button" ajax-link href="/logout">Logout</a>
+                <a href="/create" class="button" ajax-link>+ Create Transaction</a>
+            </div>
+        </nav>
 
         <div class="loading-ring">
             <div></div>

index.php
@@ -6,6 +6,9 @@
         <title>Expensify Take-Home Challenge</title>
         <link rel="stylesheet" type="text/css" href="/frontend/styles.css" />
         <link rel="icon" href="/frontend/icon.svg" type="image/svg+xml" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Nicholas Ferrara's submission for the
+        Expensify Take Home Challenge.">
     </head>
 
     <body>

commit ad1513f7b088ba4b32691ea452a14824fd2998ac

fix: css regression in bc41da78208f7cc1f0eb0232810d67785c3d38f9

frontend/js/views/BaseView.js
@@ -22,8 +22,10 @@ export class BaseView {
         document.querySelector("#modal").innerHTML = `
             <div class="modal-background">
                 <div class="modal-foreground ${small ? "small-modal" : ""}">
-                    <hr>
-                    <span id="modal-close" class="close">&times;</span>
+                    <div class="modal-top-bar">
+                        <hr>
+                        <span id="modal-close" class="close">&times;</span>
+                    </div>
                     <div id="modal-insert">${html}</div>
                 </div>
             </div>

frontend/styles.css
@@ -176,7 +176,6 @@ table td {
     border-top: 0.5em solid;
 }
 
-/* select sortable headers without data-sort-dir on hover */
 .sortable-header:hover:not([data-sort-dir])::after {
     border-bottom: 0.5em solid;
     color: var(--background-grey);
@@ -262,6 +261,11 @@ thead {
 
 .small-modal {
     max-width: 365px;
+}
+
+.modal-top-bar {
+    display: block;
+    overflow: hidden;
 }
 
 .close {